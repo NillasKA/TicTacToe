b18744ed8f2f0d3e1c218049df1b852ec7e777d7
Daniel <72373873+Daniel12345rs01@users.noreply.github.com>
2023-10-10 10:51:00 +0200

    Drag and drop

--- tictactoe/gui/controller/TicTacViewController.java
+++ tictactoe/gui/controller/TicTacViewController.java
@@ -9,16 +9,21 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.input.*;
 import javafx.scene.layout.GridPane;
 import tictactoe.bll.GameBoard;
 import tictactoe.bll.IGameModel;
 import tictactoe.gui.TicTacToe;
+
+import javax.swing.*;
 
 /**
  *
@@ -41,14 +46,86 @@ public class TicTacViewController implements Initializable
     //private IGameModel game;
     private IGameModel game = new GameBoard(new Button[]{btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9});
 
-
+    private Button clickedButton;
 
 
     TicTacMenuViewController menuController;
     TicTacToe ticTacToe; //Controller
     private static String TXT_PLAYER1  = "Player 1", TXT_PLAYER2 = "Player 2";
+    private String draggedItem;
+    private int tictoctacCounter = 0;
 
-    private IGameModel game;
+
+
+
+    @FXML
+    private void onDragDetected(MouseEvent event) {
+        clickedButton = (Button) event.getSource(); // Get the button that triggered the event
+        draggedItem = clickedButton.getText(); // Get the text of the button
+        String draggedItemChecker = null;
+
+        int player = game.getNextPlayer();
+        System.out.println(game.getNextPlayer());
+        if (tictoctacCounter >= 6 && !draggedItem.equals("")) { //If there is a item and all 6 is set
+
+            if (player == 0) {
+                draggedItemChecker = "X";
+            }
+            if (player == 1) {
+                draggedItemChecker = "O";
+            }
+
+
+            if (draggedItemChecker.equals(draggedItem)) {
+                Dragboard db = clickedButton.startDragAndDrop(TransferMode.MOVE); //Gets the moved value
+                ClipboardContent content = new ClipboardContent(); //Create a clipboard
+                content.putString(draggedItem); //Put the item in the clipboard that mean its string
+                db.setContent(content);
+                event.consume();
+
+            }
+        }
+    }
+
+
+    @FXML
+    private void onDragOver(DragEvent event) {
+        if (event.getGestureSource() != event.getSource() && event.getDragboard().hasString()) {
+            event.acceptTransferModes(TransferMode.MOVE);
+        }
+        event.consume();
+    }
+
+    @FXML
+    private void onDragDropped(DragEvent event) {
+        Button targetButton = (Button) event.getSource(); // Find out what button is the target of the drop
+        Dragboard db = event.getDragboard(); // Get the data from the clipboard
+        boolean success = false;
+
+        if (db.hasString()) { // Check if the data is a string
+            String draggedText = db.getString();  // Get the text from the clipboard
+            targetButton.setText(draggedText); // Add the dragged text to the target button
+
+            // Clear the text from the source button (assuming sourceButton is another Button)
+            clickedButton.setText("");
+            setPlayer();
+            success = true;
+        }
+
+        if (game.isGameOver())
+        {
+            int winner = game.getWinner();
+            displayWinner(winner);
+        }
+
+        event.setDropCompleted(success);
+        event.consume();
+    }
+
+
+
+
+
 
 
     @FXML
@@ -63,6 +140,7 @@ public class TicTacViewController implements Initializable
             int c = (col == null) ? 0 : col;
             int player = game.getNextPlayer();
             System.out.println(c + " " + r);
+            System.out.println(player);
             if (game.play(c, r))
             {
                 if (game.isGameOver())
@@ -72,11 +150,16 @@ public class TicTacViewController implements Initializable
                 }
                 else
                 {
-                    Button btn = (Button) event.getSource();
-                    String xOrO = player == 0 ? "X" : "O";
-                    btn.setText(xOrO);
-                    setPlayer();
-
+                    if (tictoctacCounter < 6){
+                        Button btn = (Button) event.getSource();
+                        String xOrO = player == 0 ? "X" : "O";
+                        tictoctacCounter++;
+                        btn.setText(xOrO);
+                        setPlayer();
+                        if (tictoctacCounter == 6) {
+                            game.getNextPlayer();
+                        }
+                    }
                 }
             }
         } catch (Exception e)
@@ -91,6 +174,7 @@ public class TicTacViewController implements Initializable
         game.newGame();
         setPlayer();
         clearBoard();
+        tictoctacCounter = 0;
     }
 
     @Override
